38c96b03fff7547dfcdab3366015651a6ef39fd9
justynalodzinska <justyna.lodzinska@smartive.app>
2024-05-16 00:52:03 +0200

    mqtt subscription added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
+import mqtt from 'mqtt';
+import { useEffect, useState } from 'react';
+
 import { mockResponseData } from '../mocks/data';
 
 function App() {
   const { category_title, points, question_content } = mockResponseData;
+  const [client, setClient] = useState<mqtt.MqttClient | null>(null);
+
+  const connectToMqtt = (
+    host: string,
+    mqttOption: mqtt.IClientOptions | undefined = undefined
+  ) => {
+    setClient(mqtt.connect(host, mqttOption));
+  };
+
+  useEffect(() => {
+    const mqttSub = (topic: string) => {
+      if (!client) return;
+      client.subscribe(topic);
+    };
+
+    if (!client) return;
+
+    client.on('connect', () => {
+      mqttSub('quiz/selected_question');
+    });
+  }, [client]);
+
+  const startConnection = () => {
+    connectToMqtt(import.meta.env.VITE_MQTT_SERVER_HOST);
+  };
 
   return (
-    <div className="jeopardy-question-container">
-      <div
-        className={
-          'flex items-center justify-center jeopardy-question-container-cat py-3'
-        }>
-        <p>
-          {category_title} - {points}
-        </p>
-      </div>
-      <div className={'jeopardy-question-container-content'}>
-        {question_content}
-      </div>
+    <div>
+      {client ? (
+        <div className="jeopardy-question-container">
+          <div
+            className={
+              'flex items-center justify-center jeopardy-question-container-cat py-3'
+            }>
+            <p>
+              {category_title} - {points}
+            </p>
+          </div>
+          <div className={'jeopardy-question-container-content'}>
+            {question_content}
+          </div>
+        </div>
+      ) : (
+        <button onClick={startConnection}>Połącz</button>
+      )}
     </div>
   );
 }
